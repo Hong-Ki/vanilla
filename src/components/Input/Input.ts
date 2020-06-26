304c7ae942058b1d6e61c9751911414f76cbd042
Hong-ki <minhk3259@makestar.com>
2020-06-27 07:50:26 +0900

    remove invalid style / change onChange event

--- src/components/Input/Input.ts
+++ src/components/Input/Input.ts
@@ -4,15 +4,18 @@ import Label from '../Label/Label';
 export interface Props extends CustomFormInput {
   placeholder?: string;
   type?: string;
-  onChange?(e: Event): void;
+  onChange?(e: InputEvent | Event): void;
+  onBlur?(e: Event): void;
 }
 export default ({
   defaultValue = '',
   type: elementType = 'text',
+  placeholder,
   label,
   id,
   name,
   onChange,
+  onBlur,
 }: Props = {}): HTMLDivElement => {
   const fragment = new DocumentFragment();
   let elementId = id;
@@ -30,8 +33,10 @@ export default ({
   if (elementId) inputElement.id = elementId;
   if (name) inputElement.name = name;
   if (defaultValue) inputElement.value = defaultValue;
+  if (placeholder) inputElement.placeholder = placeholder;
   if (elementType) inputElement.setAttribute('type', elementType);
-  if (onChange) inputElement.addEventListener('change', onChange);
+  if (onChange) inputElement.addEventListener('input', onChange);
+  if (onBlur) inputElement.addEventListener('blur', onBlur);
   fragment.appendChild(inputElement);
 
   const wrapperElement = document.createElement('div');
