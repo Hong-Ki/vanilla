4c055785c2da34809ed60220f826679134e26d15
Hong-ki <minhk3259@makestar.com>
2020-06-27 00:58:40 +0900

    add custom form input type

--- src/typings/declarations.d.ts
+++ src/typings/declarations.d.ts
@@ -2,3 +2,11 @@ declare module '*.scss' {
   export const content: { [className: string]: string };
   export default styles;
 }
+
+declare interface CustomFormInput {
+  defaultValue?: string;
+  label?: string;
+  id?: string;
+  name?: string;
+  onChange?(e: Event): void;
+}
