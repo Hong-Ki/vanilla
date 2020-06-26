4f2cddf2bcc4ab1bc2208e7b455452d09bc6ec2d
Hong-ki <minhk3259@makestar.com>
2020-06-27 07:52:29 +0900

    add App

--- src/App.ts
+++ src/App.ts
@@ -1,14 +1,20 @@
-import Selectbox, { Option } from './components/Selectbox/Selectbox';
-import { TELECOMS } from './constants/telecoms';
+import Header from './components/Header/Header';
+import IndentityForm from './container/IdentityForm/IdentityForm';
 
 export default () => {
   const app = document.createElement('main');
-  const selectbox = Selectbox(
-    TELECOMS.map<Option>(({ description, code }) => ({
-      name: description,
-      value: code,
-    })),
+  const HeaderComponent = Header(
+    '입력 정보',
+    '본인 명의의 휴대폰 정보를 입력하세요.',
   );
-  app.appendChild(selectbox);
+  const FormCotainer = IndentityForm({
+    onSubmit: data => {
+      console.log(data);
+    },
+  });
+
+  app.appendChild(HeaderComponent);
+  app.appendChild(FormCotainer);
+
   return app;
 };
