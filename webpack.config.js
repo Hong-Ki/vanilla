1264d104be8ec1d6b7a08ba29d1e6132474e79b3
Hong-ki <minhk3259@makestar.com>
2020-06-27 09:36:03 +0900

    add build mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,71 +2,80 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(scss|sass)$/,
-        exclude: /\.module\.(scss|sass)$/,
-        use: [
-          'style-loader',
-          { loader: 'css-loader', options: { sourceMap: true } },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: [require('autoprefixer')],
+module.exports = (env, { mode }) => {
+  const isDev = mode !== 'production';
+
+  const config = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(scss|sass)$/,
+          exclude: /\.module\.(scss|sass)$/,
+          use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            { loader: 'css-loader', options: { sourceMap: true } },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [require('autoprefixer')],
+              },
             },
-          },
-          {
-            loader: 'sass-loader',
-            options: { sourceMap: true },
-          },
-        ],
-      },
-      {
-        test: /\.module\.(scss|sass)$/,
-        use: [
-          'style-loader',
-          { loader: 'css-loader', options: { sourceMap: true, modules: true } },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: [require('autoprefixer')],
+            {
+              loader: 'sass-loader',
+              options: { sourceMap: true },
             },
-          },
-          {
-            loader: 'sass-loader',
-            options: { sourceMap: true },
-          },
-        ],
-      },
+          ],
+        },
+        {
+          test: /\.module\.(scss|sass)$/,
+          use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: { sourceMap: true, modules: true },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [require('autoprefixer')],
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: { sourceMap: true },
+            },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js', '.scss'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        minify: {
+          collapseWhitespace: true,
+        },
+        hash: true,
+        template: './static/index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.scss'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      minify: {
-        collapseWhitespace: true,
-      },
-      hash: true,
-      template: './static/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'public'),
+    },
+  };
+
+  return config;
 };
