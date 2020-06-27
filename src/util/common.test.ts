b37e7b5a0a0f721e5835d47c2a0e25a909746515
Hong-ki <minhk3259@makestar.com>
2020-06-27 09:36:17 +0900

    add test

---
+++ src/util/common.test.ts
@@ -0,0 +1,36 @@
+import { createId, isAllTrueMap, parseEventTargetToElement } from './common';
+
+const testPrefix = 'test';
+describe('createId Test', () => {
+  test('prefix Check', () => {
+    const id = createId(testPrefix);
+
+    expect(id.startsWith(testPrefix)).toBeTruthy();
+  });
+  test('id length test', () => {
+    expect(createId(testPrefix).length).toBe(testPrefix.length + 9);
+  });
+});
+
+describe('isAllTruMap Test', () => {
+  test('return true Check', () => {
+    const testMap = new Map();
+    testMap.set('1', true);
+    testMap.set('2', true);
+    testMap.set('3', true);
+    testMap.set('4', true);
+    testMap.set('5', true);
+
+    expect(isAllTrueMap(testMap)).toBeTruthy();
+  });
+  test('return false Check', () => {
+    const testMap = new Map();
+    testMap.set('1', true);
+    testMap.set('2', true);
+    testMap.set('3', true);
+    testMap.set('4', false);
+    testMap.set('5', true);
+
+    expect(isAllTrueMap(testMap)).toBeFalsy();
+  });
+});
